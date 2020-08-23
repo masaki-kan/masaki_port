a5cddece5bea2a847f88baf547fc329e803439d3
masaki-kan <63283812+masaki-kan@users.noreply.github.com>
2020-08-23 18:35:59 +0900

    initial commit

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -9,9 +9,8 @@
 <div class="c_flex">
   <div class="c_flex_l_content">
 <p id="map_image">
-
-  <img src="data:image/jpeg;base64,{{ $map->image }}"></p>
-
+  
+  <img src="{{ asset(　'storage/mapimage/' . $map->image　) }}"></p>
   </div>
 
 <div class="c_flex_r_content">
